b1b461f63bc25fb6b47a0d004f4d6f537c15c4d8
Carvane <niedziel2001@gmail.com>
2026-07-21 00:41:07 +0200

    agent

--- agent.py
+++ agent.py
@@ -1,2 +1,17 @@
+
+
 class Agent:
-    pass
+    def __init__(
+        self,
+        ConsumerKey,
+        ConsumerKeySecret,
+        AccessToken,
+        AccessTokenSecret
+    ):
+        self.__ConsumerKey = ConsumerKey
+        self.__ConsumerKeySecret = ConsumerKeySecret
+        self.__AccessToken = AccessToken
+        self.__AccessTokenSecret = AccessTokenSecret
+
+    def getPost(self):
+        print("test")
--- main.py
+++ main.py
@@ -1,9 +1,21 @@
+import os
+
+from dotenv import load_dotenv
+
 from agent import Agent
 from view import menu, menu_option, clear
 
-
 def main():
     menu()
+    xas = Agent(
+        ConsumerKey=os.getenv("CONSUMER_KEY"),
+        ConsumerKeySecret=os.getenv("CONSUMER_KEY_SECRET"),
+        AccessToken=os.getenv("ACCESS_TOKEN"),
+        AccessTokenSecret=os.getenv("ACCESS_TOKEN_SECRET")
+    )
+    xas.getPost()
+
 
 if __name__ == "__main__":
+    load_dotenv()
     main()
