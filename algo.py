3f8c9599658999dcc48a275bb1a197ebcad34965
Carvane <niedziel2001@gmail.com>
2026-07-26 22:35:09 +0200

    save

--- algo.py
+++ algo.py
@@ -1,17 +1,20 @@
-def calcRating(
+def calculateElo(
+        version: int,
         views: int,
         reposts: int,
         likes: int,
         bookmarks: int,
         replies: int,
-):
-    return (
-        views * 0.01 +
-        reposts * 100 +
-        likes * 10 +
-        bookmarks * 1000 +
-        replies * 100        
-    )
+):  
+    match version:
+        case 1:
+            return (
+                views * 0.01 +
+                reposts * 100 +
+                likes * 10 +
+                bookmarks * 1000 +
+                replies * 100        
+            )
 
 
 #print(calcRating(views=1000, reposts=1, likes=5, bookmarks=10, replies=5)) #test
---
+++ data.py
@@ -0,0 +1,47 @@
+import json
+
+def createAgent(
+        nameAgent: str,
+        model: str | None = None,
+        instruction: str | None = None,
+        enablePosting: bool | None = None,
+        imageGeneration: bool | None = None,
+        conversationId: int | None = None,
+        algorithm: str | None = None,
+        maxPostAgeHours: int | None = None,
+        minElo: int | None = None,
+        calculateEloVersion: int | None = None,
+    ):
+    PATH_DATA = "data/agent.json"
+
+    with open(PATH_DATA, "r", encoding="utf-8") as file:
+        agents = json.load(file)
+
+    if nameAgent in agents:
+        print(f'Agent "{nameAgent}" already exists.')
+        return
+
+    agents[nameAgent] = {
+        "config": {
+            "model": model,
+            "instruction": instruction,
+            "enablePosting": enablePosting,
+            "imageGeneration": imageGeneration,
+            "conversationId": conversationId,
+            "searchOptions": {
+                "algorithm": algorithm,
+                "maxPostAgeHours": maxPostAgeHours,
+                "minElo": minElo,
+                "calculateEloVersion": calculateEloVersion,
+                "monitoredAccounts": f"ma/{nameAgent}.json",
+                "processedPostIds": f"psi/{nameAgent}.json"
+            }
+        },
+        "secret": f".secret/{nameAgent}.json"
+    }
+
+    with open(PATH_DATA, "w", encoding="utf-8") as file:
+        json.dump(agents, file, indent=4)
+
+
+createAgent("testowy")
